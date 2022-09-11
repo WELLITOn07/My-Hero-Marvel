026e5215d594b2d093f349a303fd95232e3210ff
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2022-09-11 14:40:07 -0300

    design
    remove spinner and change font size

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -3,9 +3,6 @@ import { CommonModule } from '@angular/common';
 import { MyHeroComponent } from './components/my-hero/my-hero.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DetailsComponent } from './components/pages/details/details.component';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-
-
 
 @NgModule({
   declarations: [
@@ -15,7 +12,6 @@ import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
   imports: [
     CommonModule,
     SharedModule,
-    MatProgressSpinnerModule
   ],
   exports: [
     MyHeroComponent,
