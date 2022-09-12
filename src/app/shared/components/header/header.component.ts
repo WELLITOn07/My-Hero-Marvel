a973f00b95ac39d073101133717b8f4e14688df5
WELLITOn07 <97143231+WELLITOn07@users.noreply.github.com>
2022-09-12 07:45:30 -0300

    update header
    add fn for remove space in the input text

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -23,12 +23,20 @@ export class HeaderComponent implements OnInit {
   //-- BUSCANDO TODOS OS OBJETOS DA API COM O MESMO FILTRO DO INPUT (NOME DO HERÓI)
   getCharacters() {
     if (this.control.value) {
+      this.trim(this.control.value)
       this.filterHeroes = this.findHeroService.getAllCharacters(String(this.control.value));
       this.filterHeroes.subscribe(element => element);
     } else {
       window.alert('Nenhum Herói selecionado!')
     }
   };
+
+  //-- FUNÇÃO P/ REMOVER ESPAÇOS NA STRING
+  trim(text: string) {
+    return text.trim();
+  };
+
+  //-- FUNÇÃO P/ RESETAR INPUT
   clear() {
     this.control.reset();
   };
